9945ea87df6d80c0ae703aa8e50bfb021235e5d2
Xero kun <elo48858@gmail.com>
2023-05-26 15:22:18 +0100

    `FEAT` Implemented a bag data structure

---
+++ data_structures/bag_ds.py
@@ -0,0 +1,54 @@
+# github repository: https://github.com/dev-xero/python-algorithms-practice-sub-1.2
+
+"""bag_ds.py
+   Python implementation of a bag data structure
+"""
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+from data_structures.linked_list_ds import Node
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+class Bag:
+    """Implementation of a bag data structure using linked-lists"""
+    def __init__(self):
+        """Setup"""
+        self._first: Node | None = None
+        self._size: int = 0
+
+    def add(self, item: str) -> None:
+        old_first = self._first
+        first = Node()
+
+        first.item = item
+        first.next = old_first
+
+        self._first = first
+
+    def is_empty(self) -> bool:
+        """Returns true if the first node is None"""
+        return self._first is None
+
+    @property
+    def size(self) -> int:
+        """Returns the size of the bag"""
+        return self._size
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+def main():
+    """Testing"""
+    print()
+
+
+# ---------------------------------------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    main()
